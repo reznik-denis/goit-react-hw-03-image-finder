4c932803c386feb8cbd5139dfad6091cc268ff90
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-12-10 21:07:27 +0200

    add loadmore

---
+++ src/Components/LoadMore/LoadMore.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import s from './LoadMore.module.css';
+
+export default function LoadMore(props) {
+    return (
+        <button type='button' className={s.Button}>Load more</button>
+    )
+}
