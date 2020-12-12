0c13c052816cbcd7fb2d6d498a70738181b3a75f
Reznik Denis <63472391+reznik-denis@users.noreply.github.com>
2020-12-12 15:29:05 +0200

    add good finder

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import Loader from "react-loader-spinner";
 import './App.css';
 import Searchbar from './Components/Searchbar/Searchbar';
 import ImageGallery from './Components/ImageGallery/ImageGallery';
 import Modal from './Components/Modal/Modal';
 import LoadMore from './Components/LoadMore/LoadMore';
+import Api from './services/FetchImages';
+import LoaderSpinner from './Components/Loader/Loader';
+import DefaultNotifeImages from './Components/DefaultNotifeImages/DefaultNotifeImages';
 import { ToastContainer } from 'react-toastify';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 class App extends Component {
   state = {
@@ -15,71 +20,86 @@ class App extends Component {
     loading: false,
     error: null,
     largeImage: '',
+    altImage: '',
     pageFetch: 1
   }
 
-  toggleModal = (largeImageURL) => {
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.search !== this.state.search) {
+      this.fetshSearch();
+    }
+  }
+
+  toggleModal = (largeImageURL, tags) => {
     this.setState(({ showModal }) => ({
       showModal: !showModal,
-      largeImage: largeImageURL
+      largeImage: largeImageURL,
+      altImage: tags
     }));
   };
 
-  formSubmitHendler = ({search}) => {
-    this.setState({ loading: true, picture: [], search});
-
-    this.fetshSearch();
+  formSubmitHendler = ({ search }) => {
+    if (search !== this.state.search) {
+      this.setState({ picture: [], search, pageFetch: 1 });
+    } else { toast.error("Вы уже ввели это имя!"); };
+    
   };
 
   fetshSearch = () => {
-    const KEY = '19046001-7d44b7f00f708df4674bb235b';
-    const URL = 'https://pixabay.com/api/';
+    this.setState({ loading: true });
     const { search, pageFetch } = this.state;
-    fetch(`${URL}?q=${search}&page=${pageFetch}&key=${KEY}&image_type=photo&orientation=horizontal&per_page=12`)
-      .then(res => {
-        if (res.ok) {
-          return res.json();
-        }
-        return Promise.reject(new Error('Картинки с таким именем отсутсвуют'))
-      }).then(pictures => this.setState(prevState => (
+    Api.fetchImages(search, pageFetch).then(pictures => this.setState(prevState => (
         { picture: ((this.state.picture === []) ? pictures.hits : ([...prevState.picture, ...pictures.hits])) }
       )))
       .catch(error => (this.setState({error})))
-      .finally(() => this.setState(prevState => ({
-        loading: false,
-        pageFetch: prevState.pageFetch + 1
-      }))); 
+      .finally(() => {
+        this.onLoadMoreSkroll();
+        this.setState(prevState => ({
+          loading: false,
+          pageFetch: prevState.pageFetch + 1
+        }))
+      }); 
   }
 
+  onLoadMoreSkroll = () => {
+    if (this.state.pageFetch >= 2) {
+      const options = {
+        top: null,
+        behavior: 'smooth',
+      };
+
+      options.top = window.pageYOffset + document.documentElement.clientHeight;
+      setTimeout(() => {
+        window.scrollTo(options);
+      }, 1000);
+    }
+  };
+
   render() {
-    const {picture, showModal, error, largeImage, pageFetch} = this.state;
+    const {picture, showModal, error, largeImage, pageFetch, search, altImage, loading} = this.state;
     return (
       <div className="App">
-        {error && <h1>{ error.massage }</h1>}
+        {error && <h1>{error.massage}</h1>}
+
         <Searchbar onSubmit={this.formSubmitHendler} />
-        {this.state.loading && <Loader
-          type="Puff"
-          color="#00BFFF"
-          height={100}
-          width={100}
-        />}
+
         <ImageGallery
           pictures={picture}
           onClick={this.toggleModal} />
-        {picture !== [] && pageFetch >= 2 && <LoadMore onClick={this.fetshSearch}/>}
+        
+        {this.state.loading && <LoaderSpinner />}
+        
+        {(picture.length !== 0) && (pageFetch >= 2) && <LoadMore onClick={this.fetshSearch} />}
+
+        {search && picture.length === 0 && loading === false && <DefaultNotifeImages />}
+        
         {showModal && <Modal onClose={ this.toggleModal }>
-          <img src={largeImage} alt=""/>
+          <img src={largeImage} alt={altImage}/>
         </Modal>}
+        
         <ToastContainer
           position="bottom-right"
           autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
         />
     </div>
   );
